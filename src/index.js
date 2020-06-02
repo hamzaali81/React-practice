a45034ea7040ce3c6be64e01bef168fbfc4dba99
hamzaali <hamza54834@gmail.com>
2020-06-02 04:08:25 -0700

    Components use

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom, { render } from 'react-dom';
+// import './index.css'
+import './index.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// first react Component
+//JSX rules
+// return Single element
+// div / section / article
+//use camelCase for html attribute
+//className instead of class
+//close every element
+//formatting
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// function Greetings(){
+//   return <section className="container"><Book /><Book /><Book /></section>
+// }
+
+// // const Person=()=><h1>Hamza Ali</h1>;
+// const Book  =()=>{
+// return(
+// <article className="containers">
+// <CoverImage />
+// <Title />
+// <Author />
+// </article>
+// )
+// }
+
+// const CoverImage = ()=>(
+
+//   <img src="https://images-na.ssl-images-amazon.com/images/I/817VE4lKPzL._AC_UL200_SR200,200_.jpg" alt="Amazon Book"/>
+// )
+
+// const Title= ()=><h1>White Fragility</h1>;
+// const Author= ()=><p>Robin DiAngelo</p>;
+
+function Person(){
+  const btn='search buuton';
+  return(
+    <section>
+      <button>{btn}</button>
+      <h2>john doe</h2>
+      <p>info</p>      
+    </section>
+  )
+}
+
+ReactDom.render(<Person />,document.getElementById("root"));
